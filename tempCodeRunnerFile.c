ccf80f79b2149f12086f00f77e2a0f6d567f491f
Aman Panwar <amanpanwar4040@gmail.com>
2024-09-11 15:20:34 +0530

    This is my POrtfolio.

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    // int a; 
+    // a = 1;
+
+    int a = 1;
+
+    // float b=1.4;
+    float b;
+    b=1.4;
+
+    // char c;
+    // c= 'a';
+
+    char c = 'a';
+}
